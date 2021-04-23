10e75a9c5f8c69b536bbf688ea5b05653097469a
Nullifier225 <55820583+Nullifier225@users.noreply.github.com>
2021-04-23 13:26:15 +0530

    downloading image

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 from flask.helpers import url_for
+import os
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,39 +17,17 @@ def login():
 def register():
     return render_template('enroll.html')
 
-# @app.route('/constructform')
-# def construct_page():
-#     return render_template('constructform.html')
-
-# @app.route('/constructform' , methods=['POST'])
-# def construction():
-#     elements = request.form.get("elements", type=int, default=0)
-#     return render_template('display.html',list = elements)
+@app.route('/enroll' , methods=[ 'GET','POST'])
+def upload():
+    target = os.path.join(APP_ROOT,'/Users/SOWMIHARI/Documents/GitHub/Traffice_Offender_Verification/fingerprint-recognition/registered')
+    file = request.files["fingerprint"]
+    filename = request.form['identity']
+    ext = file.filename[-4:]
+    destination = "/".join([target,filename+ext])
+    print(destination)
+    file.save(destination)
     
-
-# @app.route('/display')
-# def display_page():
-#     return render_template('display.html')
-
-# @app.route('/menu')
-# def menu_page():
-#     return render_template('menu.html')
-
-# @app.route('/insert')
-# def insert_page():
-#     return render_template('insertform.html')
-
-# @app.route('/delete')
-# def delete_page():
-#     return render_template('deleteform.html')
-
-# @app.route('/search')
-# def search_page():
-#     return render_template('searchform.html')
-
-# @app.route('/viewadt')
-# def viewadt():
-#     return render_template('view_adt.html')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
